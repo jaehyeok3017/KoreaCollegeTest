063a1991b5c71ae2c808506ccded40de1e3f92c1
최재혁 <gghh042404@gmail.com>
2023-06-08 09:51:49 +0900

    Math
    gaygay

---
+++ Assets/2_Math/GeneratorOfMath.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorOfMath : MonoBehaviour
+{
+    public List<GameObject> objectsListOfMath = new List<GameObject>();
+
+
+    void Start()
+    {
+        
+    }
+
+    int time = 0;
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        time++;
+    }
+}
