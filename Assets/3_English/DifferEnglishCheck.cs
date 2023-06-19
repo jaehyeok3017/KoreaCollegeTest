312b92e93f8d938417f31c543369e678e037e0e8
최재혁 <gghh042404@gmail.com>
2023-06-19 16:00:10 +0900

    Feat : `3_English`

--- Assets/3_English/DifferEnglishCheck.cs
+++ Assets/3_English/DifferEnglishCheck.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System;
 
@@ -9,50 +10,76 @@ public class DifferEnglishCheck : MonoBehaviour
     public Sprite changeImage;
     public int englishScore = 0;
 
-    Image presentImage;
-    System.Random rand = new System.Random();
+    public List<GameObject> imgList = new List<GameObject>();
+    public GameObject presentImage;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        presentImage = GetComponent<Image>();
+        cardChange();
         timerStart(5);
-
     }
 
-    void imageChange()
+    public Image img;
+    public Sprite afterImg;
+
+    void cardChange()
     {
-        presentImage.sprite = changeImage;
+        System.Random rand = new System.Random(); 
+        
+        Sprite[] sprites = Resources.LoadAll<Sprite>("images");
+        int index = rand.Next(16);
+        imgList.sprite = sprites[index];
+
+        for (int i = 0; i < 16; i++)
+        {
+            if (randNum == i)
+            {
+                imgList[i].sprite = afterImg;
+            }
+
+            else
+            {
+
+            }
+        }
     }
 
+    public float limitTime;
+    public bool timer = false;
     void timerStart(int sec)
     {
-        
-    }
-
-    void correctImageClicked(int alongSec)
-    {
-
-    }
-
-    void wrongImageClicked(int alongSec)
-    {
-
-    }
-
-    void timeOut(int alongSec)
-    {
-
-    }
-
-    void gameReset()
-    {
-
+        limitTime = 4;
+        timer = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer)
+        {
+            limitTime -= Time.deltaTime;
+            if(limitTime < 0)
+            {
+                gameReset();
+            }
+        }
+    }
+
+    void correctImageClicked(int alongSec)
+    {
+        englishScore += 20;
+        gameReset();
+    }
+
+    void wrongImageClicked(int alongSec)
+    {
+        gameReset();
+    }
+
+    void gameReset()
+    {
+        SceneManager.LoadScene("MiddleScene");
     }
 }
