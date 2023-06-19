db29763de581978a1685a4680b58e9382818d03c
김도완 <80760639+plmkns@users.noreply.github.com>
2023-06-19 15:52:58 +0900

    all22222222

---
+++ Assets/Middle/ManagerOfEnd.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerOfEnd : MonoBehaviour
+{
+
+    public Text scoreOfKorean;
+    public Text scoreOfMath;
+    public Text scoreOfEnglish;
+    public Text scoreOfHistory;
+    public Text scoreOfJob;
+    public Text scoreOfArab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreOfMath.text = MiddleManager.testScoreOfKorean.ToString() + "점";
+        scoreOfKorean.text = MiddleManager.testScoreOfMath.ToString() + "점";
+        scoreOfEnglish.text = MiddleManager.testScoreOfEnglish.ToString() + "점";
+        scoreOfHistory.text = MiddleManager.testScoreOfHistory.ToString() + "점";
+        scoreOfJob.text = MiddleManager.testScoreOfJob.ToString() + "점";
+        scoreOfArab.text = MiddleManager.testScoreOfArab.ToString() + "점";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
