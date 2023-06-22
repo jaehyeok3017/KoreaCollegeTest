ad64cd02ba20b74d20a2b202cb3ea0359b2e8c22
최재혁 <gghh042404@gmail.com>
2023-06-22 10:35:34 +0900

    adfsljafsdmn

--- Assets/5_Job/number.cs
+++ Assets/5_Job/number.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class number : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string answer;
+    public bool isAnswer;
+
+    private GameObject target;
+
+    /*// Start is called before the first frame update
     void Start()
     {
-        
+        if(GameObject.Find("3") )
     }
+
+    public void OnClickButton()
+    {
+        if (isAnswer)
+        {
+            DifferEnglishCheck.gameReset(true);
+        }
+
+        else
+        {
+            SceneManager.LoadScene("MiddleScene");
+        }
+    }
+    */
 
     // Update is called once per frame
     void Update()
